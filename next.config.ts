75e70b115a844e0e420a677d12dda0da7a60d7cc
MAYANK RAJ <mayankrajdto@gmail.com>
2025-11-07 05:58:20 +0000

    https://drive.google.com/file/d/1FmU1XK_j1j8Ou--PQRw8iWOii5AoSzyS/view?u

--- next.config.ts
+++ next.config.ts
@@ -29,6 +29,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
